449d49d35aabf2e00b458db6c52b847a6b7bf10e
crosstyan <crosstyan@outlook.com>
2023-06-20 19:34:34 +0800

    fix: spiBegin

--- src/driver/llcc68/src/Hal.cpp
+++ src/driver/llcc68/src/Hal.cpp
@@ -3,13 +3,16 @@
 #include "ch32v003fun.h"
 #include "ch32v003_SPI.h"
 #include "system_tick.h"
-#include "printf.h"
 #include "gpio.h"
 #include "Hal.h"
 
-void RadioLibHal::init() {}
+void RadioLibHal::init() {
+  this->spiBegin();
+}
 
-void RadioLibHal::term() {}
+void RadioLibHal::term() {
+  this->spiEnd();
+}
 
 inline void RadioLibHal::pinMode(uint32_t pin, uint32_t mode) {
   GPIO::pinMode(pin, static_cast<GPIO::PinMode>(mode));
@@ -34,15 +37,12 @@ inline void RadioLibHal::spiBegin() {
   SPI_begin_8();
 }
 
-
-
 inline uint8_t RadioLibHal::spiTransfer(uint8_t b) {
   return SPI_transfer_8(b);
 }
 
 // not sure... leave it for now
 void RadioLibHal::spiBeginTransaction() {}
-// not sure... leave it for now
 void RadioLibHal::spiEndTransaction() {}
 
 void RadioLibHal::spiEnd() {}
